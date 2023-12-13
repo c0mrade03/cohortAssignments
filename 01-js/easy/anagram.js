49b39e66d7c8c248ef6e9a3a9397530daf429f0d
Prathamesh <devpsk03@gmail.com>
2023-12-14 01:26:49 +0530

    wrote code of anagram and expenditure

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,29 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  const charCountMap = new Map();
 
+  for (const char of str1) {
+    if (char !== ' ') {
+      charCountMap.set(char, (charCountMap.get(char) || 0) + 1);
+    }
+  }
+
+  for (const char of str2) {
+    if (char !== ' ') {
+      charCountMap.set(char, (charCountMap.get(char) || 0) - 1);
+    }
+  }
+
+  for (const count of charCountMap.values()) {
+    if (count !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
