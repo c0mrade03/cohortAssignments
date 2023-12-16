f900706113c8c8b867a66ba2d1059b6a6df32b4d
Prathamesh <devpsk03@gmail.com>
2023-12-16 16:45:58 +0530

    Modified some codes from week 1 Assignments

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -1,12 +1,16 @@
 /*
-  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
-  Example:
-  - Input: [3, 7, 2, 9, 1]
-  - Output: 9
+  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
 */
 
 function findLargestElement(numbers) {
-    
+    if (numbers.length == 0) {
+        return undefined;
+    }
+    const ans = Math.max(...numbers);
+    return ans;
 }
 
 module.exports = findLargestElement;
--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,26 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function calculateTime(n, fn) {
+    fn(n);
 }
+function calculateTimeUsingDate(n) {
+    const start = new Date();
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    const end = new Date();
+    console.log("Using Date:",(end - start) / 1000);
+}
+function calculateTimeUsingPerformance(n) {
+    const start = performance.now();
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    const end = performance.now();
+    console.log("Using Performance:",(end - start) / 1000);
+}
+calculateTime(100, calculateTimeUsingDate);
+calculateTime(100, calculateTimeUsingPerformance);
